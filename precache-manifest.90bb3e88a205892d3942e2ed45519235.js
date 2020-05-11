0d50496a1da0d6108383c2d86b07e2b09ba2f6ee
https://github.com/mishhubc <mihaijicum@gmail.com>
2020-05-11 19:19:36 +0200

    Updates

---
+++ precache-manifest.90bb3e88a205892d3942e2ed45519235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e3c450245459e361933e4e09ea40e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "752a393406a43701ab29",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "149a2f710bbc7bcd73cc",
+    "url": "/static/js/2.ab602cd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab602cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752a393406a43701ab29",
+    "url": "/static/js/main.e066219b.chunk.js"
+  },
+  {
+    "revision": "3335eaa5095e605079c7",
+    "url": "/static/js/runtime-main.322fc38c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
